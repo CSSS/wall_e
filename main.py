53683a6e0390d256b1fd88f396df7f011afae512
Winfield Chen <wnfldchen@gmail.com>
2018-07-08 18:15:28 -0700

    Decode redis output as string.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ TOKEN = os.environ['TOKEN']
 
 bot = commands.Bot(command_prefix='.')
 
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
+r = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
 
 message_queue = queue.Queue()
 
