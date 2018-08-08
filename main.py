78b369654a6c428a842e88cbe9bac612b8b8ed67
Winfield Chen <wnfldchen@gmail.com>
2018-08-08 02:53:58 -0700

    Attempt to fix double load bug.

--- main.py
+++ main.py
@@ -203,5 +203,7 @@ async def urban(ctx, queryString):
         link = "\n <" + data[1]['permalink'] + ">"
         await ctx.send("```" + str + "```" + "\n*Link* " + link)
 
-bot.load_extension('testenv')
+if __name__ == '__main__':
+    bot.load_extension('testenv')
+
 bot.run(TOKEN)
