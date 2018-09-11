aa815baa7a18a7546d03e394c5c97e85f4c09fa1
titanvj <vijenderbakhshi@gmail.com>
2018-09-11 12:49:00 -0700

    added settings.py to hold global var for the bot name and avatar. reasons being nothing else worked.

---
+++ helper_files/settings.py
@@ -0,0 +1,10 @@
+# TODO: Come up with better name for this file
+# Use this hold globals and other things need across the cogs
+# that cannot be initialized for whatever reason in the cogs themselves
+
+# To be initialed from main in the on_ready function
+# Used in almost all the cogs for author name and author avatar in embeds
+global BOT_NAME
+BOT_NAME = ''
+global BOT_AVATAR
+BOT_AVATAR = ''
