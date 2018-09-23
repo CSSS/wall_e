58491464d06d73918d8f0b0a27d527548d02d76d
modernNeo <j_manshad@sfu.ca>
2018-09-22 17:24:20 -0700

    redoing a push as the server doesnt have the latest code

--- commands_to_load/HealthChecks.py
+++ commands_to_load/HealthChecks.py
@@ -67,8 +67,8 @@ class HealthChecks():
 			
 		logger.info("[HealthChecks help()] transfer successful")
 
-		#rolesList = sorted(rolesList, key=str.lower)
 		await paginateEmbed(bot=self.bot,title="Help Page" ,ctx=ctx,listToEmbed=helpArr, numOfPageEntries=5, add_field=True)
+		#rolesList = sorted(rolesList, key=str.lower)
 
 
 def setup(bot):
