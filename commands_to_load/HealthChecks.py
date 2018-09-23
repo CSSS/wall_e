6c488206a90bc9a8ac094a906123c3708348d7d7
modernNeo <j_manshad@sfu.ca>
2018-09-22 17:04:12 -0700

    updating file from VJ's copy and then modifying a parameter

--- commands_to_load/HealthChecks.py
+++ commands_to_load/HealthChecks.py
@@ -2,6 +2,8 @@ from discord.ext import commands
 import discord.client
 import json
 from helper_files.Paginate import paginateEmbed, paginate
+from helper_files.embed import embed 
+import helper_files.settings as settings
 
 import logging
 logger = logging.getLogger('wall_e')
@@ -14,17 +16,21 @@ class HealthChecks():
 	@commands.command()
 	async def ping(self, ctx):
 		logger.info("[HealthChecks ping()] ping command detected from "+str(ctx.message.author))
-		await ctx.send('```pong!```')
+		eObj = embed(description='Pong!', author=settings.BOT_NAME, avatar=settings.BOT_AVATAR)
+		await ctx.send(embed=eObj)
 
 
 	@commands.command()
 	async def echo(self, ctx, *args):
-		user = ctx.author.nick or ctx.author.name
+		user = ctx.author.display_name
 		arg=''
 		for argument in args:
 			arg+=argument+' '
 		logger.info("[HealthChecks echo()] echo command detected from "+str(ctx.message.author)+" with argument "+str(arg))
-		await ctx.send(user + " says: " + arg)
+		avatar = ctx.author.avatar_url
+		eObj = embed(author=user, avatar=avatar, description=arg)
+		
+		await ctx.send(embed=eObj)
 
 	@commands.command()
 	async def help(self, ctx):
@@ -62,7 +68,7 @@ class HealthChecks():
 		logger.info("[HealthChecks help()] transfer successful")
 
 		#rolesList = sorted(rolesList, key=str.lower)
-		await paginateEmbed(bot=self.bot,title="Help Page" ,ctx=ctx,listToEmbed=helpArr, numOfPageEntries=5)
+		await paginateEmbed(bot=self.bot,title="Help Page" ,ctx=ctx,listToEmbed=helpArr, numOfPageEntries=5, add_field=True)
 
 
 def setup(bot):
