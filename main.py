a88ad28f02ef805d90db7a9c974b139e8d7d96f5
modernNeo <j_manshad@sfu.ca>
2018-09-27 22:33:26 -0700

    adding more debugging

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ async def on_member_join(member):
         output+="\tYou can give yourself a class role by running <.iam cmpt320> or create a new class by <.newclass cmpt316>\n"
         output+="\tPlease keep Academic Honesty in mind when discussing course material here.\n"
 
-        eObj = imported_embed(title="Welcome to the SFU CSSS's Discord Channel", author=settings.BOT_NAME, avatar=settings.BOT_AVATAR, description=output)
+        eObj = imported_embed(title="Welcome to the SFU CSSS's "+str(os.environ['BRANCH'])+" Discord Channel", author=settings.BOT_NAME, avatar=settings.BOT_AVATAR, description=output)
         await member.send(embed=eObj,content=None)
         logger.info("[main.py on_member_join] embed sent to member "+str(member))
 
