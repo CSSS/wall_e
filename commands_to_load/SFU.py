efedad79e363dcd83e5da25e4464cd986a67598b
VJ <vijenderbakhshi@gmail.com>
2018-09-28 10:58:59 -0700

    added method for determind term

--- commands_to_load/SFU.py
+++ commands_to_load/SFU.py
@@ -16,6 +16,17 @@ class SFU():
     @commands.command()
     async def sfu(self, ctx, *course):
         logger.info('[SFU sfu()] sfu command detected from user ' + str(ctx.message.author))
+
+        year = time.localtime()[0]
+        term = time.localtime()[1]
+
+        if(term <= 4):
+            term = 'spring'
+        elif(term >= 5 and term <= 8):
+            term = 'summer'
+        else:
+            term = 'fall'
+        
         
 def setup(bot):
     bot.add_cog(SFU(bot))
