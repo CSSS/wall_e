2e284040e989adc8670ef2a19093c4e4f9ae622b
Jace Manshadi <jaymanshadi@gmail.com>
2019-01-12 03:08:47 -0800

    trying to debug why line throws syntax error

--- commands_to_load/Reminders.py
+++ commands_to_load/Reminders.py
@@ -44,8 +44,7 @@ class Reminders():
 				logger.info("[Reminders __init__] branch is =["+branch+"]")
 				reminder_channel = discord.utils.get(self.bot.guilds[0].channels, name=branch + '_reminder_channel')
 				if reminder_channel is None:
-					log_channel = await self.bot.guilds[0].create_text_channel(branch + '_reminder_channel')
-					reminder_channel = await self.bot.guilds[0].create_text_channel(branch + '_reminder_channel')
+					reminder_channel = await bot.guilds[0].create_text_channel(branch + '_reminder_channel')
 				REMINDER_CHANNEL_ID = reminder_channel.id
 			self.channel = self.bot.get_channel(REMINDER_CHANNEL_ID) # channel ID goes here
 			logger.info("[Reminders __init__] variable \"BOT_LOG_CHANNEL\" is set to \""+str(BOT_LOG_CHANNEL)+"\"")
