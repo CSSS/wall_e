978476f6327256e679e3428933b10e70ebbcc2d1
Jace Manshadi <jaymanshadi@gmail.com>
2019-03-01 22:51:40 -0800

    updating the ordering of the settings.py file and removing the global keyword as it seems unncessary

--- helper_files/settings.py
+++ helper_files/settings.py
@@ -1,15 +1,14 @@
-# TODO: Come up with better name for this file
-# Use this hold globals and other things need across the cogs
-# that cannot be initialized for whatever reason in the cogs themselves
+# hold all variables that are needed across the cogs
+
+
+import os, json
+
+
 
 # To be initialed from main in the on_ready function
 # Used in almost all the cogs for author name and author avatar in embeds
-global BOT_NAME
 BOT_NAME = ''
-global BOT_AVATAR
 BOT_AVATAR = ''
-
-import os, json
 
 ##################################
 ## ENVIRONMENT VARIABLES TO USE ##
