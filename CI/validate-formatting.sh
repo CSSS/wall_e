6d94578d99d24664bdfecd886deda9fa69cddd90
Jace Manshadi <jaymanshadi@gmail.com>
2019-09-21 22:08:15 -0700

    fixing multi-line shell command

--- CI/validate-formatting.sh
+++ CI/validate-formatting.sh
@@ -8,7 +8,9 @@ docker image rm -f ${pyTestContainerNameLowerCase}
 docker build -t ${pyTestContainerNameLowerCase} \
     --build-arg UNIT_TEST_RESULTS=mount/unit_results.xml  -f Dockerfile.test .
 mkdir -p ${UNIT_TEST_RESULTS}
-docker run -d -e --net=host --name ${pyTestContainerName} ${pyTestContainerNameLowerCase} --mount type=bind,source="${UNIT_TEST_RESULTS}",target=${UNIT_TEST_RESULTS}
+docker run -d --mount type=bind,source="${UNIT_TEST_RESULTS}",target=${UNIT_TEST_RESULTS} \
+    --net=host --name ${pyTestContainerName} ${pyTestContainerNameLowerCase}
+
 sleep 20
 docker inspect ${pyTestContainerName} --format='{{.State.ExitCode}}' | grep  '0'
 testContainerFailed=$?
