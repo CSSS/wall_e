6cf2cd22ac8bf1bf65e7b1913a081bb969c3e64f
Jace Manshadi <jaymanshadi@gmail.com>
2019-09-22 00:07:11 -0700

    trying to figure out why dockerfile cant touch the requirements file

--- CI/validate-formatting.sh
+++ CI/validate-formatting.sh
@@ -11,6 +11,7 @@ cmd="docker build -t ${pyTestContainerNameLowerCase} \
     --build-arg DOCKER_CONTAINER_TEST_RESULT_DIRECTORY=${DOCKER_CONTAINER_TEST_RESULT_DIRECTORY} \
     --build-arg UNIT_TEST_RESULTS=${UNIT_TEST_RESULTS} -f Dockerfile.test ."
 echo $cmd
+ls -l
 $cmd
 
 echo "first step"
