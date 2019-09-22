e7fd5e2828dbf1fb31f5bbf96aeaf2dcc007464b
Jace Manshadi <jaymanshadi@gmail.com>
2019-09-21 20:54:41 -0700

    improving how errors are outputted

--- CI/validate-formatting.sh
+++ CI/validate-formatting.sh
@@ -14,7 +14,7 @@ if [ "${testContainerFailed}" -eq "1" ]; then
     discordOutput=$(docker logs ${pyTestContainerName} | tail -12)
     output=$(docker logs ${pyTestContainerName})
     printf $discordOutput > wall_e_file
-    echo $output
+    echo -e $output
     exit 1
 fi
 printf "successful" > wall_e_file
