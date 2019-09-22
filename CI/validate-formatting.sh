3680887d4f52251c284dcd93f4b12d3c58e1dc1c
Jace Manshadi <jaymanshadi@gmail.com>
2019-09-21 22:35:38 -0700

    debugging

--- CI/validate-formatting.sh
+++ CI/validate-formatting.sh
@@ -11,20 +11,31 @@ cmd="docker build -t ${pyTestContainerNameLowerCase} \
 echo $cmd
 $cmd
 
+echo "first step"
 mkdir -p ${UNIT_TEST_RESULTS}
+echo "second step"
 docker run -d \
     --mount type=bind,source="${WORKSPACE}/${UNIT_TEST_RESULTS}",target="${DOCKER_CONTAINER_TEST_RESULT_DIRECTORY}/${UNIT_TEST_RESULTS}" \
     --net=host --name ${pyTestContainerName} ${pyTestContainerNameLowerCase}
 
+echo "third step"
 sleep 20
+echo "fourth step"
 docker inspect ${pyTestContainerName} --format='{{.State.ExitCode}}' | grep  '0'
 testContainerFailed=$?
+echo "fifth step"
 if [ "${testContainerFailed}" -eq "1" ]; then
+    echo "sixth step"
     discordOutput=$(docker logs ${pyTestContainerName} | tail -12)
+    echo "seventh step"
     output=$(docker logs ${pyTestContainerName})
+    echo "eighth step"
     printf $discordOutput > wall_e_file
+    echo "ninth step"
     echo -e $output
+    echo "tenth step"
     exit 1
 fi
+echo "eleventh step"
 printf "successful" > wall_e_file
 exit 0
