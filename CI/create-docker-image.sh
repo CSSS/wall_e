827413ae83718f58b066a3649ceb277bc6655875
Jace Manshadi <jaymanshadi@gmail.com>
2019-09-22 04:04:05 -0700

    change so that jenkins will build the repo

--- CI/create-docker-image.sh
+++ CI/create-docker-image.sh
@@ -11,6 +11,17 @@ DOCKERFILE="Dockerfile.walle.base"
 past_2_commits=($(git log -2 --pretty=format:"%h"))
 files_changed=($(git diff --name-only $(echo ${past_2_commits[*]})))
 
+docker image rm -f wall_e || true
+cmd="docker build -t ${IMAGENAME} \
+    --build-arg CONTAINER_HOME_DIR=${CONTAINER_HOME_DIR} \
+    --build-arg UNIT_TEST_RESULTS=${UNIT_TEST_RESULTS} -f ${DOCKERFILE}. ."
+echo $cmd
+$cmd
+docker tag ${IMAGENAME} ${DOCKERREGISTRY}/${IMAGENAME}
+docker tag ${IMAGENAME} ${DOCKERREGISTRY}/${IMAGENAME}:${VERSION}
+docker push ${DOCKERREGISTRY}/${IMAGENAME}
+docker push ${DOCKERREGISTRY}/${IMAGENAME}:${VERSION}
+
 for file_changed in "${files_changed[@]}"
 do
     if [ "${file_changed}" == "requirements.txt" ]; then
