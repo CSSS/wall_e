3ca1b75bc673548d59c2ef92dfd5e189507abe3d
Jace Manshadi <jaymanshadi@gmail.com>
2019-11-08 15:15:50 -0800

    updating steps for building the pytest container

--- wall_e/test/validate-formatting.sh
+++ wall_e/test/validate-formatting.sh
@@ -3,26 +3,27 @@ set -e -o xtrace
 
 whoami
 
-pyTestContainerName="${COMPOSE_PROJECT_NAME}XXwall_e_pytest"
+pyTestContainerName="${DOCKER_TEST_IMAGE}"
 ls -l
 ./wall_e/test/lineEndings.sh
 docker rm -f ${pyTestContainerName} || true
 pyTestContainerNameLowerCase=$(echo "$pyTestContainerName" | awk '{print tolower($0)}')
 docker image rm -f ${pyTestContainerNameLowerCase}
-cmd="docker build -t ${pyTestContainerNameLowerCase} \
-    --build-arg CONTAINER_HOME_DIR=${CONTAINER_HOME_DIR} \
-    --build-arg UNIT_TEST_RESULTS=${UNIT_TEST_RESULTS} -f Dockerfile.test ."
-echo $cmd
-$cmd
-
 mkdir -p ${UNIT_TEST_RESULTS}
-cmd="docker run -d \
+
+
+docker build -t ${pyTestContainerNameLowerCase} \
+    -f CI/Dockerfile.test \
+    --build-arg CONTAINER_HOME_DIR=${CONTAINER_HOME_DIR} \
+    --build-arg UNIT_TEST_RESULTS=${CONTAINER_TEST_DIR}  .
+
+docker run -d \
+    -v ${LOCALHOST_SRC_DIR}:{CONTAINER_SRC_DIR} \
     --mount \
-    type=bind,source="${WORKSPACE}/${UNIT_TEST_RESULTS}",target="${CONTAINER_HOME_DIR}/${UNIT_TEST_RESULTS}" \
-    --net=host --name ${pyTestContainerName} ${pyTestContainerNameLowerCase}"
-echo $cmd
-$cmd
+    type=bind,source="${LOCALHOST_TEST_DIR}",target="${CONTAINER_TEST_DIR}" \
+    --net=host --name ${DOCKER_TEST_CONTAINER} ${pyTestContainerNameLowerCase}
 sleep 20
+
 docker inspect ${pyTestContainerName} --format='{{.State.ExitCode}}' | grep  '0'
 testContainerFailed=$?
 if [ "${testContainerFailed}" -eq "1" ]; then
