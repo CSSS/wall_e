ba28de9a105617cd41f57e2c2e361e36dd44a0f3
Jace Manshadi <jaymanshadi@gmail.com>
2019-11-08 09:31:54 +0000

    will stop writing to the bot log channel is the runtime error is experienced

--- wall_e/resources/utilities/log_channel.py
+++ wall_e/resources/utilities/log_channel.py
@@ -67,6 +67,9 @@ async def write_to_bot_log_channel(bot, config, f):
                             lastIndex += 2000
                             if len(output[firstIndex:lastIndex]) == 0:
                                 finished = True
+                    except RuntimeError as run:
+                        logger.info("[main.py write_to_bog_log_channek] experienced RuntimeError, will assume that an authorized uer exited")
+                        break
                     except Exception as exc:
                         exc_str = '{}: {}'.format(type(exc).__name__, exc)
                         logger.error('[main.py write_to_bot_log_channel] write to channel failed\n{}'.format(exc_str))
