b9d3a736e9ed7fd0c1e54a0d3c55ef39d46d2584
Jace Manshadi <jaymanshadi@gmail.com>
2019-11-09 01:53:49 -0800

    fixing docker command

--- wall_e/test/validate-formatting.sh
+++ wall_e/test/validate-formatting.sh
@@ -20,22 +20,22 @@ echo LOCALHOST_SRC_DIR=${LOCALHOST_SRC_DIR}
 echo LOCALHOST_TEST_DIR=${LOCALHOST_TEST_DIR}
 echo DOCKER_TEST_IMAGE=${DOCKER_TEST_IMAGE}
 echo DOCKER_TEST_CONTAINER=${DOCKER_TEST_CONTAINER}
-pyTestContainerNameLowerCase=$(echo "$DOCKER_TEST_IMAGE" | awk '{print tolower($0)}')
+DOCKER_TEST_IMAGE=$(echo "$DOCKER_TEST_IMAGE" | awk '{print tolower($0)}')
 
-docker rm -f ${pyTestContainerNameLowerCase} || true
+docker rm -f ${DOCKER_TEST_CONTAINER} || true
 docker image rm -f ${DOCKER_TEST_IMAGE}
 
 rm -r ${LOCALHOST_TEST_DIR} || true
 mkdir -p ${LOCALHOST_TEST_DIR}
 
 
-docker build -t ${pyTestContainerNameLowerCase} \
+docker build -t ${DOCKER_TEST_IMAGE} \
     -f CI/Dockerfile.test \
     --build-arg CONTAINER_HOME_DIR=${CONTAINER_HOME_DIR} \
     --build-arg UNIT_TEST_RESULTS=${CONTAINER_TEST_DIR}  .
 
 docker run -d \
-    --name ${DOCKER_TEST_CONTAINER} ${pyTestContainerNameLowerCase}
+    --name ${DOCKER_TEST_CONTAINER} ${DOCKER_TEST_IMAGE}
 #    -v ${LOCALHOST_SRC_DIR}:${CONTAINER_SRC_DIR} \
 #    --volumes-from csss_jenkins \
 #    -v ${LOCALHOST_TEST_DIR}:${CONTAINER_TEST_DIR} \
@@ -43,11 +43,11 @@ docker run -d \
 sleep 20
 sudo docker cp ${DOCKER_TEST_CONTAINER}:${CONTAINER_TEST_DIR}/all-unit-tests.xml ${LOCALHOST_TEST_DIR}/stuff.xml
 
-docker inspect ${DOCKER_TEST_IMAGE} --format='{{.State.ExitCode}}' | grep  '0'
+docker inspect ${DOCKER_TEST_CONTAINER} --format='{{.State.ExitCode}}' | grep  '0'
 
 testContainerFailed=$?
 if [ "${testContainerFailed}" -eq "1" ]; then
-    discordOutput=$(docker logs ${DOCKER_TEST_IMAGE} | tail -12)
+    discordOutput=$(docker logs ${DOCKER_TEST_CONTAINER} | tail -12)
     printf $discordOutput > ${RESULT_FILE}
     exit 1
 fi
