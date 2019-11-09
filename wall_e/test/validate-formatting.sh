f199a07cc0e2caa081b4aec347741dccd471c165
Jace Manshadi <jaymanshadi@gmail.com>
2019-11-08 20:11:23 -0800

    trying to fix error with env variables

--- wall_e/test/validate-formatting.sh
+++ wall_e/test/validate-formatting.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 set -e -o xtrace
 
-whoami
-
-export CONTAINER_TEST_DIR=${CONTAINER_HOME_DIR}/tests
-export CONTAINER_SRC_DIR=${CONTAINER_HOME_DIR}/src
-
-export CONTAINER_TEST_DIR=${CONTAINER_HOME_DIR}/tests
-export CONTAINER_SRC_DIR=${CONTAINER_HOME_DIR}/src
-
-export DOCKER_TEST_IMAGE=${COMPOSE_PROJECT_NAME}_wall_e_pytest
-export DOCKER_TEST_CONTAINER=${COMPOSE_PROJECT_NAME}_pytest
-
-echo LOCALHOST_TEST_DIR=${LOCALHOST_TEST_DIR}
-
 ./wall_e/test/lineEndings.sh
 
-pyTestContainerName="${DOCKER_TEST_IMAGE}"
-docker rm -f ${pyTestContainerName} || true
-pyTestContainerNameLowerCase=$(echo "$pyTestContainerName" | awk '{print tolower($0)}')
+docker rm -f ${DOCKER_TEST_IMAGE} || true
+pyTestContainerNameLowerCase=$(echo "$DOCKER_TEST_IMAGE" | awk '{print tolower($0)}')
 docker image rm -f ${pyTestContainerNameLowerCase}
-mkdir -p ${UNIT_TEST_RESULTS}
+mkdir -p ${CONTAINER_TEST_DIR}
 
 
 docker build -t ${pyTestContainerNameLowerCase} \
@@ -35,10 +21,10 @@ docker run -d \
     --net=host --name ${DOCKER_TEST_CONTAINER} ${pyTestContainerNameLowerCase}
 sleep 20
 
-docker inspect ${pyTestContainerName} --format='{{.State.ExitCode}}' | grep  '0'
+docker inspect ${DOCKER_TEST_IMAGE} --format='{{.State.ExitCode}}' | grep  '0'
 testContainerFailed=$?
 if [ "${testContainerFailed}" -eq "1" ]; then
-    discordOutput=$(docker logs ${pyTestContainerName} | tail -12)
+    discordOutput=$(docker logs ${DOCKER_TEST_IMAGE} | tail -12)
     printf $discordOutput > ${RESULT_FILE}
     exit 1
 fi
