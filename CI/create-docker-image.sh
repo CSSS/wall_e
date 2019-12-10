39f76f01a84e51b285e0890d92bf85fc88251f10
Jace Manshadi <jaymanshadi@gmail.com>
2019-12-09 18:33:54 -0800

    fixing the requirements so it finds the right requirements

--- CI/create-docker-image.sh
+++ CI/create-docker-image.sh
@@ -12,9 +12,9 @@ DOCKERFILE="CI/Dockerfile.requirements"
 past_2_commits=($(git log -2 --pretty=format:"%h"))
 files_changed=($(git diff --name-only $(echo ${past_2_commits[*]})))
 
-for file_changed in "${files_changed[@]}"
-do
-    if [ "${file_changed}" == "wall_e/src/requirements.txt" ]; then
+#for file_changed in "${files_changed[@]}"
+#do
+#    if [ "${file_changed}" == "wall_e/src/requirements.txt" ]; then
         echo "${DOCKER_HUB_PASSWORD}" | docker login --username=${DOCKER_HUB_USER_NAME} --password-stdin
         docker image rm -f wall_e || true
         docker build -t ${IMAGENAME} \
@@ -25,5 +25,5 @@ do
         docker push ${DOCKERREGISTRY}/${IMAGENAME}
         docker push ${DOCKERREGISTRY}/${IMAGENAME}:${VERSION}
         exit 0
-    fi
-done
+#    fi
+#done
