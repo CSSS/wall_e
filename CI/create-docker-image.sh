595d19a7697fe19f97881ce24f796b60bff69ce4
Jace Manshadi <jaymanshadi@gmail.com>
2019-12-09 18:30:50 -0800

    fixing variable calling

--- CI/create-docker-image.sh
+++ CI/create-docker-image.sh
@@ -15,7 +15,7 @@ files_changed=($(git diff --name-only $(echo ${past_2_commits[*]})))
 for file_changed in "${files_changed[@]}"
 do
     if [ "${file_changed}" == "wall_e/src/requirements.txt" ]; then
-        echo "{DOCKER_HUB_PASSWORD}" | docker login --username=${DOCKER_HUB_USER_NAME} --password-stdin
+        echo "${DOCKER_HUB_PASSWORD}" | docker login --username=${DOCKER_HUB_USER_NAME} --password-stdin
         docker image rm -f wall_e || true
         docker build -t ${IMAGENAME} \
             --build-arg CONTAINER_HOME_DIR=${CONTAINER_HOME_DIR} \
