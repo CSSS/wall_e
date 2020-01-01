0ba71d7c7d587db7792fa418c9dde0812116eaf3
Jace Manshadi <jaymanshadi@gmail.com>
2020-01-01 02:33:11 -0800

    updating variable name

--- wall_e/src/resources/utilities/list_of_perms.py
+++ wall_e/src/resources/utilities/list_of_perms.py
@@ -4,6 +4,6 @@ import inspect
 
 async def get_list_of_user_permissions(ctx, userID=False):
     if userID is not False:
-        return [ attribute[0] for attribute in inspect.getmembers(ctx.guild.get_member(id).guild_permissions) if not attribute[0].startswith('_') and not inspect.ismethod(attribute[1]) and attribute[1] == True]
+        return [ perm[0] for perm in inspect.getmembers(ctx.guild.get_member(id).guild_permissions) if not perm[0].startswith('_') and not inspect.ismethod(perm[1]) and perm[1] == True]
     else:
-        return [ attribute[0] for attribute in inspect.getmembers(ctx.author.guild_permissions) if not attribute[0].startswith('_') and not inspect.ismethod(attribute[1]) and attribute[1] == True]
+        return [ perm[0] for perm in inspect.getmembers(ctx.author.guild_permissions) if not perm[0].startswith('_') and not inspect.ismethod(perm[1]) and perm[1] == True]
