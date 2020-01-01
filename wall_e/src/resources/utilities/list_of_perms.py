68eab08d9b7730e413190b8155ea2784e6c440c6
Jace Manshadi <jaymanshadi@gmail.com>
2020-01-01 07:21:54 -0800

    fixing flake8 issue

--- wall_e/src/resources/utilities/list_of_perms.py
+++ wall_e/src/resources/utilities/list_of_perms.py
@@ -2,8 +2,11 @@
 # https://discordpy.readthedocs.io/en/rewrite/api.html#discord.Permissions
 import inspect
 
+
 async def get_list_of_user_permissions(ctx, userID=False):
     if userID is not False:
-        return [ perm[0] for perm in inspect.getmembers(ctx.guild.get_member(id).guild_permissions) if not perm[0].startswith('_') and not inspect.ismethod(perm[1]) and perm[1] == True]
+        return [perm[0] for perm in inspect.getmembers(ctx.guild.get_member(id).guild_permissions)
+                if not perm[0].startswith('_') and not inspect.ismethod(perm[1]) and perm[1]]
     else:
-        return [ perm[0] for perm in inspect.getmembers(ctx.author.guild_permissions) if not perm[0].startswith('_') and not inspect.ismethod(perm[1]) and perm[1] == True]
+        return [perm[0] for perm in inspect.getmembers(ctx.author.guild_permissions)
+                if not perm[0].startswith('_') and not inspect.ismethod(perm[1]) and perm[1]]
