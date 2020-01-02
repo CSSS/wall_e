ed12e0b132d439826a1b7ee2d544df40af6b9f3e
Jace Manshadi <jaymanshadi@gmail.com>
2020-01-01 16:05:47 -0800

    adding more logging for determine what perm is aquired

--- wall_e/src/resources/utilities/list_of_perms.py
+++ wall_e/src/resources/utilities/list_of_perms.py
@@ -1,12 +1,19 @@
 # list of possible roles to check can be pulled from here
 # https://discordpy.readthedocs.io/en/rewrite/api.html#discord.Permissions
 import inspect
+import logging
+logger = logging.getLogger('wall_e')
 
 
 async def get_list_of_user_permissions(ctx, userID=False):
     if userID is not False:
-        return [perm[0] for perm in inspect.getmembers(ctx.guild.get_member(id).guild_permissions)
-                if not perm[0].startswith('_') and not inspect.ismethod(perm[1]) and perm[1]]
+        perms = [perm[0] for perm in inspect.getmembers(ctx.guild.get_member(id).guild_permissions)
+                 if not perm[0].startswith('_') and not inspect.ismethod(perm[1]) and perm[1]]
+        logger.info("[list_of_perms.py get_list_of_user_permissions()] permissions for {} is {}".format(
+            ctx.guild.get_member(id), perms))
     else:
-        return [perm[0] for perm in inspect.getmembers(ctx.author.guild_permissions)
-                if not perm[0].startswith('_') and not inspect.ismethod(perm[1]) and perm[1]]
+        perms = [perm[0] for perm in inspect.getmembers(ctx.author.guild_permissions)
+                 if not perm[0].startswith('_') and not inspect.ismethod(perm[1]) and perm[1]]
+        logger.info("[list_of_perms.py get_list_of_user_permissions()] permissions for {} is {}".format(
+            ctx.author, perms))
+    return perms
