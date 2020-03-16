02347fd6b522e03436fa0cafb5fcb4ffa3b1a392
Jace Manshadi <jaymanshadi@gmail.com>
2020-03-15 20:15:14 -0700

    removing origin name for dockerfile since all version of test wall_e will not need to rebuild their own version of the base image

--- CI/server_scripts/build_wall_e/deploy-to-test-server.sh
+++ CI/server_scripts/build_wall_e/deploy-to-test-server.sh
@@ -12,8 +12,6 @@ export test_container_name="${COMPOSE_PROJECT_NAME}_wall_e"
 export test_image_name_lower_case=$(echo "$test_container_name" | awk '{print tolower($0)}')
 export compose_project_name_lower_case=$(echo "$COMPOSE_PROJECT_NAME" | awk '{print tolower($0)}')
 export docker_compose_file="CI/server_scripts/build_wall_e/docker-compose.yml"
-export ORIGIN_IMAGE="sfucsssorg/wall_e"
-
 
 docker rm -f ${test_container_name} ${test_container_db_name} || true
 docker network rm ${compose_project_name_lower_case}_default || true
