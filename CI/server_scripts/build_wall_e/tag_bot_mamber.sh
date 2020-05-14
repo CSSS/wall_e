868997abac177fd7aa68f2d032d716a4cb7732c4
Jace Manshadi <jaymanshadi@gmail.com>
2020-05-14 15:00:35 -0700

    updating script used tag the bot_managers

---
+++ CI/server_scripts/build_wall_e/tag_bot_mamber.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+bot_token="{$1}"
+bot_manager_id_csss_guild="<@&321832268282855436>"
+
+content="**New PR to Reviews**\n PR: # : ${pr_url} \nRequired Reviewers: ${bot_manager_id_csss_guild} "
+
+#my guild
+bot_management_channel_id=466734608726229005
+
+curl --header "Content-Type: application/json" \
+ --request POST -H "Authorization: Bot ${bot_token}" \
+   --data "{\"content\":\"${content}\"}" \
+   https://discordapp.com/api/channels/${bot_management_channel_id}/messages
