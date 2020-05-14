a4338c23d0fbaede84d9387472ea83062ba980cc
Jace Manshadi <jaymanshadi@gmail.com>
2020-05-14 16:06:04 -0700

    updating tagging

--- CI/server_scripts/build_wall_e/tag_bot_member.sh
+++ CI/server_scripts/build_wall_e/tag_bot_member.sh
@@ -3,9 +3,10 @@
 bot_token="${1}"
 bot_manager_id_csss_guild="<@&321832268282855436>"
 bot_manager_id_csss_guild="<@&710627204970840074>"
-pr_url="${2}"
+pr_number="${2}"
+pr_url="${3}"
 
-content="**New PR to Reviews**\n PR: # : ${pr_url} \nRequired Reviewers: ${bot_manager_id_csss_guild} "
+content="**New PR to Reviews**\n PR # ${pr_number} \n${pr_url} \n${bot_manager_id_csss_guild} "
 
 #my guild
 bot_management_channel_id=466734608726229005
