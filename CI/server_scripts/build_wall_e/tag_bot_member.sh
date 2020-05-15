c904850187020d922f3b38a52e233c23e7a8032e
Jace Manshadi <jaymanshadi@gmail.com>
2020-05-14 18:21:01 -0700

    some more temp changes

--- CI/server_scripts/build_wall_e/tag_bot_member.sh
+++ CI/server_scripts/build_wall_e/tag_bot_member.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+
+get_guild_roles(){
+  /guilds/{guild.id}/roles
+
+}
 bot_token="${1}"
 bot_manager_id_csss_guild="<@&321832268282855436>"
 bot_manager_id_csss_guild="<@&710627204970840074>"
 pr_number="${2}"
 pr_url="${3}"
 
-content="**New PR to Reviews**\n PR # ${pr_number} \n${pr_url} \n${bot_manager_id_csss_guild} "
+content="**New PR to Review**\n PR# ${pr_number} \n${pr_url} \n${bot_manager_id_csss_guild} "
 
 #my guild
 bot_management_channel_id=466734608726229005
