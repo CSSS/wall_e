b2fb3dffdb1d41e6678bfacea5786ae2068e0666
Vijender Bakhshi <vbakhshi@sfu.ca>
2020-08-23 13:04:00 -0700

    Vj frosh game embed cmd (#312)
    * updated dev and production ini files for the new cog
    
    * new cog frosh with command for enabling team creating within discord
    
    * updated help.json with new class and command
    
    * updates for linter
    
    * updates for linter+1
    
    * added a try catch around the colour setting bit
    
    * some updates to the help text
    
    * added some test cases to the Test_Cases.md file
    
    * moved command usage to help.json and replaced error embed with a general error message and pointed to the help command.
    
    * linting fix on frosy.py
    
    * converted all .format strings to f-strings
    
    * removed str() conversion redundancy
    
    * put the new frosh cog option in the .ini files in alphabetical order
    
    * updated command name to have it better associated with Frosh
    
    * added team as an alias to the froshteam command

---
+++ wall_e/src/resources/cogs/frosh.py
@@ -0,0 +1,58 @@
+from discord.ext import commands
+import logging
+from resources.utilities.embed import embed as em
+logger = logging.getLogger('wall_e')
+
+
+class Frosh(commands.Cog):
+
+    def __init__(self, bot, config):
+        self.bot = bot
+        self.config = config
+
+    @commands.command(aliases=["team"])
+    async def froshteam(self, ctx, *info):
+        logger.info(f'[Frosh team()] team command detected from user {ctx.author}')
+        logger.info(f'[Frosh team()] arguments given: {info}')
+
+        if len(info) < 3:
+            e_obj = await em(
+                ctx,
+                title='Missing Arguments',
+                author=self.config.get_config_value('bot_profile', 'BOT_NAME'),
+                avatar=self.config.get_config_value('bot_profile', 'BOT_AVATAR'),
+                colour=0xA6192E,
+                content=[('Error', 'You are missing arguments. Call `.help team` for how to use the command')],
+                footer='Team Error'
+            )
+            await ctx.send(embed=e_obj)
+            logger.info('[Frosh team()] Missing arguments, command ended')
+            return
+
+        # just gonna assume the provided stuff is all good
+        e_obj = await em(
+            ctx,
+            title='CSSS Frosh 2020 Gaming Arena',
+            author=ctx.author.display_name,
+            avatar=ctx.author.avatar_url,
+            content=[
+                    ('Team Name', info[0]),
+                    ('Game', info[1]),
+                    ('Contact', ctx.author.mention),
+                    ('Team Members', '\n'.join(list(map(lambda str: str.strip(), info[2].split(',')))))
+                    ],
+            footer='Frosh 2020'
+            )
+
+        try:
+            if len(info) >= 4:
+                color = info[3]
+                if color[0] == '#':
+                    color = color[1:]
+                e_obj.colour = int('0x'+color, base=16)
+        except Exception:
+            pass
+
+        logger.info(f'[Frosh team()] team embed created with the following fields: {e_obj.fields}')
+
+        await ctx.send(embed=e_obj)
