b32a6469e521b5ab93b7684016d496dc6817f647
Jace Manshadi <jmanshad@protonmail.com>
2021-08-16 00:29:34 -0700

    fixing flake8 issues

--- wall_e/src/resources/cogs/role_commands.py
+++ wall_e/src/resources/cogs/role_commands.py
@@ -226,7 +226,7 @@ class RoleCommands(commands.Cog):
     @commands.command()
     async def whois(self, ctx, role_to_check):
         if f"{role_to_check}" == "Muted" and ctx.message.author not in \
-            discord.utils.get(ctx.guild.roles, name="Minions").members:
+          discord.utils.get(ctx.guild.roles, name="Minions").members:
             await ctx.send("no peaking at the muted folks!")
             return
         if ctx.channel.id != self.bot_channel.id:
