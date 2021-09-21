8f57d76537f4dbd241bcb8d11bceb37e40c4c4f9
Jace Manshadi <jmanshad@protonmail.com>
2021-09-20 18:29:17 -0700

    updating the documentation and how migration are handled in the cloud

---
+++ CI/server_scripts/build_wall_e/wait-for-postgres.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# wait-for-postgres.sh
+
+# aquired from https://docs.docker.com/compose/startup-order/
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+
+PGPASSWORD=$POSTGRES_PASSWORD psql --set=WALL_E_DB_USER="${WALL_E_DB_USER}" \
+  --set=WALL_E_DB_PASSWORD="${WALL_E_DB_PASSWORD}"  --set=WALL_E_DB_DBNAME="${WALL_E_DB_DBNAME}" \
+  -h "$host" -U "postgres" -f WalleModels/create-database.ddl
+
+python3 django-db-orm-manage.py migrate
+
+exec $cmd
+
