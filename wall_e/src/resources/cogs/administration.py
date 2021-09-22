dd266cfca60c3a98c7ad3ac4bcac94b4c6e90f46
Jace Manshadi <jmanshad@protonmail.com>
2021-09-22 00:50:58 -0700

    fixing command for showing the number of stats when using more than 1 page

--- wall_e/src/resources/cogs/administration.py
+++ wall_e/src/resources/cogs/administration.py
@@ -12,6 +12,7 @@ import pytz
 import requests
 from discord.ext import commands
 
+import django_db_orm_settings
 from WalleModels.models import CommandStat, Reminder
 from resources.utilities.send import send as helper_send
 
@@ -58,7 +59,7 @@ class Administration(commands.Cog):
         """
         commands = False
         reminders = False
-        tz = pytz.timezone("America/Vancouver")
+        tz = pytz.timezone(django_db_orm_settings.TIME_ZONE)
         await ctx.send("trying to get sql file")
         try:
             sql_lines = requests.get(f"{ctx.message.attachments[0].url}").text.split("\r\n")
@@ -67,7 +68,7 @@ class Administration(commands.Cog):
             return
         await ctx.send("sql file received, will now iterate over its lines")
         for line in sql_lines:
-            if 'COPY public.commandstats' in line:
+            if 'COPY public.commandstats ' in line:
                 commands = True
             elif 'COPY public.reminders ' in line:
                 reminders = True
@@ -77,22 +78,38 @@ class Administration(commands.Cog):
                 line = line.split("\t")
                 if commands:
                     try:
+                        epoch_time = int(line[0])
+                        year = int(line[1])
+                        month = int(line[2])
+                        day = int(line[3])
+                        hour = int(line[4])
+                        channel_name = line[5]
+                        command = line[6]
+                        invoked_with = line[7]
+                        invoked_subcommand = line[8]
                         await CommandStat.save_command_async(
                             CommandStat(
-                                epoch_time=line[0], year=line[1], month=line[2], day=line[3],
-                                hour=line[4], channel_name=line[5], command=line[6],
-                                invoked_with=line[7], invoked_subcommand=line[8]
+                                epoch_time=epoch_time, year=year, month=month, day=day, hour=hour,
+                                channel_name=channel_name, command=command, invoked_with=invoked_with,
+                                invoked_subcommand=invoked_subcommand
                             )
                         )
                     except Exception:
                         print(f"unable to save commandStat {line}")
                 elif reminders:
-                    aware = tz.localize(datetime.datetime.strptime(f"{line[1]}", "%Y-%m-%d %H:%M:%S.%f"), is_dst=None)
+
                     try:
+                        date_to_be_reminded_epoch = line[1]
+                        message = line[2]
+                        author_id = int(line[3])
+                        date_to_be_reminded_epoch = tz.localize(
+                            datetime.datetime.strptime(f"{date_to_be_reminded_epoch}", "%Y-%m-%d %H:%M:%S.%f"),
+                            is_dst=None
+                        )
                         await Reminder.save_reminder(
                             Reminder(
-                                reminder_date_epoch=aware.timestamp(), message=line[2], author_id=line[3],
-                                author_name=line[4], message_id=line[5]
+                                reminder_date_epoch=date_to_be_reminded_epoch.timestamp(), message=message,
+                                author_id=author_id
                             )
                         )
                     except Exception:
@@ -106,13 +123,13 @@ class Administration(commands.Cog):
 
     @commands.command()
     async def load(self, ctx, name):
-        logger.info("[Administration load()] load command detected from {}".format(ctx.message.author))
+        logger.info(f"[Administration load()] load command detected from {ctx.message.author}")
         valid, folder = self.valid_cog(name)
         if not valid:
-            await ctx.send("```{} isn't a real cog```".format(name))
+            await ctx.send(f"```{name} isn't a real cog```")
             logger.info(
-                "[Administration load()] {} tried loading "
-                "{} which doesn't exist.".format(ctx.message.author, name)
+                f"[Administration load()] {ctx.message.author} tried loading "
+                f"{name} which doesn't exist."
             )
             return
         try:
@@ -120,37 +137,37 @@ class Administration(commands.Cog):
             cog_class_name = inspect.getmembers(sys.modules[cog_file.__name__], inspect.isclass)[0][0]
             cog_to_load = getattr(cog_file, cog_class_name)
             self.bot.add_cog(cog_to_load(self.bot, self.config))
-            await ctx.send("{} command loaded.".format(name))
-            logger.info("[Administration load()] {} has been successfully loaded".format(name))
+            await ctx.send(f"{name} command loaded.")
+            logger.info(f"[Administration load()] {name} has been successfully loaded")
         except(AttributeError, ImportError) as e:
-            await ctx.send("command load failed: {}, {}".format(type(e), str(e)))
-            logger.info("[Administration load()] loading {} failed :{}, {}".format(name, type(e), e))
+            await ctx.send(f"command load failed: {type(e)}, {e}")
+            logger.info(f"[Administration load()] loading {name} failed :{type(e)}, {e}")
 
     @commands.command()
     async def unload(self, ctx, name):
-        logger.info("[Administration unload()] unload command detected from {}".format(ctx.message.author))
+        logger.info(f"[Administration unload()] unload command detected from {ctx.message.author}")
         valid, folder = self.valid_cog(name)
         if not valid:
-            await ctx.send("```{} isn't a real cog```".format(name))
+            await ctx.send(f"```{name} isn't a real cog```")
             logger.info(
-                "[Administration load()] {} tried loading "
-                "{} which doesn't exist.".format(ctx.message.author, name)
+                f"[Administration load()] {ctx.message.author} tried loading "
+                f"{name} which doesn't exist."
             )
             return
         cog_file = importlib.import_module(folder + name)
         cog_class_name = inspect.getmembers(sys.modules[cog_file.__name__], inspect.isclass)[0][0]
         self.bot.remove_cog(cog_class_name)
-        await ctx.send("{} command unloaded".format(name))
-        logger.info("[Administration unload()] {} has been successfully loaded".format(name))
+        await ctx.send(f"{name} command unloaded")
+        logger.info(f"[Administration unload()] {name} has been successfully loaded")
 
     @commands.command()
     async def reload(self, ctx, name):
-        logger.info("[Administration reload()] reload command detected from {}".format(ctx.message.author))
+        logger.info(f"[Administration reload()] reload command detected from {ctx.message.author}")
         valid, folder = self.valid_cog(name)
         if not valid:
-            await ctx.send("```{} isn't a real cog```".format(name))
-            logger.info("[Administration reload()] {} tried "
-                        "loading {} which doesn't exist.".format(ctx.message.author, name))
+            await ctx.send(f"```{name} isn't a real cog```")
+            logger.info(f"[Administration reload()] {ctx.message.author} tried "
+                        f"loading {name} which doesn't exist.")
             return
         cog_file = importlib.import_module(folder + name)
         cog_class_name = inspect.getmembers(sys.modules[cog_file.__name__], inspect.isclass)[0][0]
@@ -158,27 +175,27 @@ class Administration(commands.Cog):
         try:
             cog_to_load = getattr(cog_file, cog_class_name)
             self.bot.add_cog(cog_to_load(self.bot, self.config))
-            await ctx.send("`{} command reloaded`".format(folder + name))
-            logger.info("[Administration reload()] {} has been successfully reloaded".format(name))
+            await ctx.send(f"`{folder + name} command reloaded`")
+            logger.info(f"[Administration reload()] {name} has been successfully reloaded")
         except(AttributeError, ImportError) as e:
-            await ctx.send("Command load failed: {}, {}".format(type(e), str(e)))
-            logger.info("[Administration reload()] loading {} failed :{}, {}".format(name, type(e), e))
+            await ctx.send(f"Command load failed: {type(e)}, {e}")
+            logger.info(f"[Administration reload()] loading {name} failed :{type(e)}, {e}")
 
     @commands.command()
     async def exc(self, ctx, *args):
         logger.info("[Administration exc()] exc command detected "
-                    "from {} with arguments {}".format(ctx.message.author, " ".join(args)))
+                    f"from {ctx.message.author} with arguments {' '.join(args)}")
         query = " ".join(args)
         # this got implemented for cases when the output of the command is too big to send to the channel
         exit_code, output = subprocess.getstatusoutput(query)
-        await helper_send(ctx, "Exit Code: {}".format(exit_code))
+        await helper_send(ctx, f"Exit Code: {exit_code}")
         await helper_send(ctx, output, prefix="```", suffix="```")
 
     @commands.command()
     async def frequency(self, ctx, *args):
         if self.config.enabled("database_config", option="DB_ENABLED"):
             logger.info("[Administration frequency()] frequency command "
-                        "detected from {} with arguments [{}]".format(ctx.message.author, args))
+                        f"detected from {ctx.message.author} with arguments [{args}]")
             column_headers = CommandStat.get_column_headers_from_database()
             if len(args) == 0:
                 await ctx.send(f"please specify which columns you want to count={column_headers}")
@@ -206,17 +223,17 @@ class Administration(commands.Cog):
                 fig, ax = self.plt.subplots()
                 y_pos = self.np.arange(len(labels))
                 for i, v in enumerate(numbers):
-                    ax.text(v, i + .25, str(v), color='blue', fontweight='bold')
+                    ax.text(v, i + .25, f"{v}", color='blue', fontweight='bold')
                 ax.barh(y_pos, numbers, align='center', color='green')
                 ax.set_yticks(y_pos)
                 ax.set_yticklabels(labels)
                 ax.invert_yaxis()  # labels read top-to-bottom
                 if len(args) > 1:
-                    title = '-'.join(str(arg) for arg in args[:len(args) - 1])
-                    title += "-{}".format(args[len(args) - 1])
+                    title = '-'.join(f"{arg}" for arg in args[:len(args) - 1])
+                    title += f"-{args[len(args) - 1]}"
                 else:
                     title = args[0]
-                ax.set_title("How may times each {} appears in the database since Sept 21, 2018".format(title))
+                ax.set_title(f"How may times each {title} appears in the database since Sept 21, 2018")
                 fig.set_size_inches(18.5, 10.5)
                 fig.savefig(image_path)
                 logger.info("[Administration frequency()] graph created and saved")
@@ -229,32 +246,45 @@ class Administration(commands.Cog):
                 if len(dic_result) % 50 != 0:
                     number_of_pages += 1
                 number_of_bars_per_page = int(len(dic_result) / number_of_pages) + 1
-                first_index, last_index = 0, number_of_bars_per_page - 1
                 msg = None
                 current_page = 0
-                while first_index < len(dic_result):
+
+                first_index = 0
+                last_index = number_of_bars_per_page - 1
+                boundaries_for_each_page = {}
+                for page in range(0, number_of_pages):
+                    boundaries_for_each_page[page] = {
+                        'first_index': first_index,
+                        'last_index': last_index
+                    }
+                    first_index += number_of_bars_per_page
+                    last_index += number_of_bars_per_page
+
+                while True:
                     logger.info("[Administration frequency()] creating "
-                                "a graph with entries {} to {}".format(first_index, last_index))
+                                f"a graph with entries {first_index} to {last_index}")
                     to_react = ['⏪', '⏩', '✅']
+                    first_index = boundaries_for_each_page[current_page]['first_index']
+                    last_index = boundaries_for_each_page[current_page]['last_index']
                     labels = [i[0] for i in dic_result][first_index:last_index]
                     numbers = [i[1] for i in dic_result][first_index:last_index]
                     self.plt.rcdefaults()
                     fig, ax = self.plt.subplots()
                     y_pos = self.np.arange(len(labels))
                     for i, v in enumerate(numbers):
-                        ax.text(v, i + .25, str(v), color='blue', fontweight='bold')
+                        ax.text(v, i + .25, f"{v}", color='blue', fontweight='bold')
                     ax.barh(y_pos, numbers, align='center', color='green')
                     ax.set_yticks(y_pos)
                     ax.set_yticklabels(labels)
                     ax.invert_yaxis()  # labels read top-to-bottom
-                    ax.set_xlabel("Page {}/{}".format(current_page, number_of_pages - 1))
+                    ax.set_xlabel(f"Page {current_page}/{number_of_pages - 1}")
                     if len(args) > 1:
-                        title = '_'.join(str(arg) for arg in args[:len(args) - 1])
-                        title += "_{}".format(args[len(args) - 1])
+                        title = '_'.join(f"{arg}" for arg in args[:len(args) - 1])
+                        title += f"_{args[len(args) - 1]}"
                     else:
                         title = args[0]
-                    ax.set_title("How may times each {} appears in the database since Sept 21, 2018".format(title))
-                    fig.set_size_inches(18.5, 10.5)
+                    ax.set_title(f"How may times each {title} appears in the database since Sept 21, 2018")
+                    fig.set_size_inches(30, 10.5)
                     fig.savefig(image_path)
                     logger.info("[Administration frequency()] graph created and saved")
                     self.plt.close(fig)
@@ -269,37 +299,29 @@ class Administration(commands.Cog):
                     def check_reaction(reaction, user):
                         if not user.bot:  # just making sure the bot doesnt take its own reactions
                             # into consideration
-                            e = str(reaction.emoji)
-                            logger.info("[Administration frequency()] reaction {} detected from {}".format(e, user))
+                            e = f"{reaction.emoji}"
+                            logger.info(f"[Administration frequency()] reaction {e} detected from {user}")
                             return e.startswith(('⏪', '⏩', '✅'))
 
                     logger.info("[Administration frequency()] graph image file has been sent")
-                    user_reacted = False
-                    while user_reacted is False:
+                    user_reacted = None
+                    while user_reacted is None:
                         try:
                             user_reacted = await self.bot.wait_for('reaction_add', timeout=20, check=check_reaction)
                         except asyncio.TimeoutError:
                             logger.info("[Administration frequency()] timed out waiting for the user's reaction.")
                         if user_reacted:
                             if '⏪' == user_reacted[0].emoji:
-                                first_index -= number_of_bars_per_page
-                                last_index -= number_of_bars_per_page
                                 current_page -= 1
-                                if first_index < 0:
-                                    first_index, last_index = number_of_bars_per_page * 3, number_of_bars_per_page * 4
+                                if current_page < 0:
                                     current_page = number_of_pages - 1
                                 logger.info("[Administration frequency()] user indicates they "
-                                            " want to go back to page "
-                                            + str(current_page))
+                                            f" want to go back to page {current_page}")
                             elif '⏩' == user_reacted[0].emoji:
-                                first_index += number_of_bars_per_page
-                                last_index += number_of_bars_per_page
                                 current_page += 1
-                                if first_index > len(dic_result):
-                                    first_index, last_index = 0, number_of_bars_per_page
+                                if current_page >= number_of_pages:
                                     current_page = 0
-                                logger.info("[Administration frequency()] user indicates they want to go to page "
-                                            + str(current_page))
+                                logger.info(f"[Administration frequency()] user indicates they want to go to page {current_page}")
                             elif '✅' == user_reacted[0].emoji:
                                 logger.info("[Administration frequency()] user "
                                             "indicates they are done with the roles "
@@ -311,4 +333,4 @@ class Administration(commands.Cog):
                             await msg.delete()
                             return
                     logger.info("[Administration frequency()] updating first_index "
-                                "and last_index to {} and {} respectively".format(first_index, last_index))
+                               f"and last_index to {first_index} and {last_index} respectively")
