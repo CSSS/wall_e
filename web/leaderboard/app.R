009df722ff1fb2150c318478916047f93bc7dfd1
Winfield Chen <wca88@sfu.ca>
2022-02-21 02:30:23 -0800

    Very primitive initial table display to be improved

--- web/leaderboard/app.R
+++ web/leaderboard/app.R
@@ -9,11 +9,16 @@ con = dbConnect(RPostgres::Postgres(),
                 password = Sys.getenv('WALL_E_DB_PASSWORD'))
 
 ui = fluidPage(
-    titlePanel("CSSS Discord Leaderboard")
+    titlePanel("CSSS Discord Leaderboard"),
+    fluidRow(column(12,dataTableOutput('table')))
 )
 
 server = function(input, output, session) {
-    
+    res = dbSendQuery(con, 'SELECT * FROM "WalleModels_userpoint" ORDER BY points DESC')
+    df = dbFetch(res)
+    dbClearResult(res)
+    output$table = renderDataTable(df,options = list(pageLength = 100))
 }
 
 shinyApp(ui = ui, server = server)
+dbDisconnect(con)
