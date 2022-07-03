5db8bbe21c5a2f4d7d7d34054cf26bf547034417
Winfield Chen <wca88@sfu.ca>
2022-07-02 19:59:09 -0700

    Add users script

---
+++ web/leaderboard/users.py
@@ -0,0 +1,15 @@
+import os
+import discord
+import pandas
+
+
+class UsersClient(discord.Client):
+    async def on_ready(self):
+        df = pandas.DataFrame(((member.id, member.name) for member in self.guilds[0].members),
+                              columns=['user_id', 'user_name'])
+        df.to_csv('users.csv', index=False)
+        await self.close()
+
+
+users_client = UsersClient(intents=discord.Intents.all())
+users_client.run(os.getenv('TOKEN'))
