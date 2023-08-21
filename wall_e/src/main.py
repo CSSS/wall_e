4ab0b53e9b385e693669c65735e1b494a9b4ea31
Jace Manshadi <jmanshad@protonmail.com>
2023-08-21 15:15:33 -0700

    enabling the listeners for slash commands now that the version has been bumped

--- wall_e/src/main.py
+++ wall_e/src/main.py
@@ -1,9 +1,13 @@
+import datetime
 import importlib
 import inspect
 import os
+import re
 import sys
 
-# import discord
+import traceback
+
+import discord
 import django
 import time
 
@@ -13,8 +17,8 @@ from django.core.wsgi import get_wsgi_application
 from resources.utilities.logger_setup import initialize_logger
 from resources.utilities.config.config import WallEConfig
 from resources.utilities.log_channel import write_to_bot_log_channel
-# from resources.utilities.slash_command_checks import _command_in_correct_test_guild_channel
-# from resources.utilities.embed import embed as imported_embed
+from resources.utilities.embed import embed as imported_embed
+from resources.utilities.slash_command_checks import command_in_correct_test_guild_channel
 
 logger, FILENAME = initialize_logger()
 wall_e_config = WallEConfig(os.environ['ENVIRONMENT'])
@@ -148,54 +152,54 @@ async def on_message(message):
     else:
         await bot.process_commands(message)
 
-#
-# ########################################################
-# # Function that gets called whenever a commmand      ##
-# # gets called, being use for data gathering purposes ##
-# ########################################################
-# @bot.event
-# async def on_app_command_completion(interaction: discord.Interaction, cmd: discord.app_commands.commands.Command):
-#     from WalleModels.models import CommandStat
-#     database_enabled = wall_e_config.enabled("database_config", option="DB_ENABLED")
-#     if _command_in_correct_test_guild_channel(wall_e_config, interaction) and database_enabled:
-#         await CommandStat.save_command_async(CommandStat(
-#             epoch_time=datetime.datetime.now().timestamp(), channel_name=interaction.channel.name,
-#             command=interaction.command.name, invoked_with=cmd.qualified_name,
-#             invoked_subcommand=cmd.qualified_name
-#         ))
-#
-#
-# ####################################################
-# # Function that gets called when the script cant ##
-# # understand the command that the user invoked   ##
-# ####################################################
-# @bot.tree.error
-# async def on_command_error(interaction: discord.Interaction, error):
-#     if _command_in_correct_test_guild_channel(wall_e_config, interaction):
-#         if isinstance(error, commands.MissingRequiredArgument):
-#             logger.error(f'[ManageCog on_command_error()] Missing argument: {error.param}')
-#             e_obj = await imported_embed(
-#                 ctx_obj=interaction.response.send_message,
-#                 author=wall_e_config.get_config_value('bot_profile', 'BOT_NAME'),
-#                 avatar=wall_e_config.get_config_value('bot_profile', 'BOT_AVATAR'),
-#                 description=f"Missing argument: {error.param}"
-#             )
-#             if e_obj is not False:
-#                 await interaction.response.send_message(embed=e_obj)
-#         elif isinstance(error, commands.errors.CommandNotFound):
-#             return
-#         else:
-#             # only prints out an error to the log if the string that was entered doesnt contain just "."
-#             pattern = r'[^\.]'
-#             if re.search(pattern, f"{error}"[9:-14]):
-#                 if type(error) is discord.ext.commands.errors.CheckFailure:
-#                     logger.warning(
-#                         f"[ManageCog on_command_error()] user {interaction.user} "
-#                         "probably tried to access a command they arent supposed to"
-#                     )
-#                 else:
-#                     traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
-#                     return
+
+########################################################
+# Function that gets called whenever a commmand      ##
+# gets called, being use for data gathering purposes ##
+########################################################
+@bot.event
+async def on_app_command_completion(interaction: discord.Interaction, cmd: discord.app_commands.commands.Command):
+    from WalleModels.models import CommandStat
+    database_enabled = wall_e_config.enabled("database_config", option="DB_ENABLED")
+    if command_in_correct_test_guild_channel(wall_e_config, interaction) and database_enabled:
+        await CommandStat.save_command_async(CommandStat(
+            epoch_time=datetime.datetime.now().timestamp(), channel_name=interaction.channel.name,
+            command=interaction.command.name, invoked_with=cmd.qualified_name,
+            invoked_subcommand=cmd.qualified_name
+        ))
+
+
+####################################################
+# Function that gets called when the script cant ##
+# understand the command that the user invoked   ##
+####################################################
+@bot.tree.error
+async def on_command_error(interaction: discord.Interaction, error):
+    if command_in_correct_test_guild_channel(wall_e_config, interaction):
+        if isinstance(error, commands.MissingRequiredArgument):
+            logger.error(f'[ManageCog on_command_error()] Missing argument: {error.param}')
+            e_obj = await imported_embed(
+                ctx_obj=interaction.response.send_message,
+                author=wall_e_config.get_config_value('bot_profile', 'BOT_NAME'),
+                avatar=wall_e_config.get_config_value('bot_profile', 'BOT_AVATAR'),
+                description=f"Missing argument: {error.param}"
+            )
+            if e_obj is not False:
+                await interaction.response.send_message(embed=e_obj)
+        elif isinstance(error, commands.errors.CommandNotFound):
+            return
+        else:
+            # only prints out an error to the log if the string that was entered doesnt contain just "."
+            pattern = r'[^\.]'
+            if re.search(pattern, f"{error}"[9:-14]):
+                if type(error) is discord.ext.commands.errors.CheckFailure:
+                    logger.warning(
+                        f"[ManageCog on_command_error()] user {interaction.user} "
+                        "probably tried to access a command they arent supposed to"
+                    )
+                else:
+                    traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+                    return
 
 ####################
 # STARTING POINT ##
