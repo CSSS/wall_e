37ea2cdf839bb43776e70fa11e35ff4038f97dd5
Jace Manshadi <jmanshad@protonmail.com>
2023-08-26 04:36:41 -0700

    fixing how to handle when no guild is found

--- wall_e/src/resources/utilities/get_guild.py
+++ wall_e/src/resources/utilities/get_guild.py
@@ -1,8 +1,9 @@
 def get_guild(bot, config):
-    if config.get_config_value("basic_config", "ENVIRONMENT") != 'TEST':
+    env = config.get_config_value("basic_config", "ENVIRONMENT")
+    if env != 'TEST':
         return bot.guild[0]
     guild_name = config.get_config_value("basic_config", "BRANCH_NAME")
     for guild in bot.guilds:
         if guild.name == guild_name:
             return guild
-    return None
+    raise Exception(f"no guild found for {env}")
