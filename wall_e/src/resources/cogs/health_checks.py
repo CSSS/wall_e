a344efd15d9ffcc9037279c8ef4a034b05fc5e13
Jace Manshadi <jmanshad@protonmail.com>
2023-08-25 21:48:06 -0700

    fixing the documentation and variable references for echo command

--- wall_e/src/resources/cogs/health_checks.py
+++ wall_e/src/resources/cogs/health_checks.py
@@ -46,17 +46,17 @@ class HealthChecks(commands.Cog):
         if e_obj is not False:
             await interaction.response.send_message(embed=e_obj)
 
-    @app_commands.command(name="echo", description="echo command")
-    @app_commands.describe(user_input="string to echo")
-    async def echo(self, interaction: discord.Interaction, user_input: str):
+    @app_commands.command(name="echo", description="repeats what the user said back at them")
+    @app_commands.describe(string="string to echo")
+    async def echo(self, interaction: discord.Interaction, string: str):
         await slash_command_checks(self.logger, self.config, interaction, self.help_dict)
         user = interaction.user.display_name
         self.logger.info("[HealthChecks echo()] echo command "
-                         "detected from {} with argument {}".format(interaction.user, user_input)
+                         "detected from {} with argument {}".format(interaction.user, string)
                          )
         avatar = interaction.user.avatar.url
         e_obj = await embed(
-            self.logger, interaction=interaction, author=user, avatar=avatar, description=user_input
+            self.logger, interaction=interaction, author=user, avatar=avatar, description=string
         )
         if e_obj is not False:
             await interaction.response.send_message(embed=e_obj)
