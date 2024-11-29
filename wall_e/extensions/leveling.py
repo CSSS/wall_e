e330f55009ba9acfa25810e46695baf6f5306e9e
Jace Manshadi <jaymanshad@proton.me>
2024-11-28 18:40:42 -0800

    found bug

--- wall_e/extensions/leveling.py
+++ wall_e/extensions/leveling.py
@@ -715,6 +715,12 @@ class Leveling(commands.Cog):
         user_ids_to_update = await UserPoint.get_users_with_expired_images()
         number_of_users_to_update = len(user_ids_to_update)
         if number_of_users_to_update == 0:
+            self.number_of_process_outdated_profile_pics_in_progress -= 1
+            self.update_outdated_profile_pics_logger.debug(
+                f"[Leveling process_outdated_profile_pics()] [exiting task] "
+                f"self.number_of_process_outdated_profile_pics_in_progress ="
+                f" {self.number_of_process_outdated_profile_pics_in_progress}"
+            )
             return
         self.update_outdated_profile_pics_logger.debug(
             f"[Leveling process_outdated_profile_pics()] {number_of_users_to_update} users with outdated CDN links"
