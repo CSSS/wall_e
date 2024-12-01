e3f63f2e741e3ad71794debd4635bb1e01f41266
Jace Manshadi <jaymanshad@proton.me>
2024-12-01 13:26:01 -0800

    [DEBUG] adding more logging to figure out why certain users cant be updated

--- wall_e/extensions/leveling.py
+++ wall_e/extensions/leveling.py
@@ -739,12 +739,15 @@ class Leveling(commands.Cog):
         :return:
         """
         if member:
+            user_point = self.user_points[member.id]
             try:
-                if self.user_points[member.id].leveling_update_attempt >= 5:
+                if user_point.leveling_update_attempt >= 5:
                     logger.error(
                         f"[Leveling _update_member_profile_data()] "
-                        f"attempt {self.user_points[member.id].leveling_update_attempt} to update the member profile"
-                        f" data in the database for member {member} with id {member.id} "
+                        f"attempt {user_point.leveling_update_attempt} to update the member profile"
+                        f" data in the database for member {member} with id [{member.id}], expiry_date of "
+                        f"[{user_point.discord_avatar_link_expiry_date.pst}] and a CDN link of"
+                        f"{user_point.leveling_message_avatar_url} "
                         f"{index + 1}/{total_number_of_updates_needed}"
                     )
                 else:
@@ -754,7 +757,7 @@ class Leveling(commands.Cog):
                         f"[Leveling _update_member_profile_data()] attempting to update the member profile data"
                         f" in the database for member {member} {index + 1}/{total_number_of_updates_needed}"
                     )
-                    user_updated = await self.user_points[member.id].update_leveling_profile_info(
+                    user_updated = await user_point.update_leveling_profile_info(
                         logger, self.guild.id, member, self.levelling_website_avatar_channel,
                         updated_user_log_id=updated_user_log_id
                     )
